a29288a807cb807902485a50f25822c633905e99
elisamartin <elisamv93@gmail.com>
2019-03-22 11:32:42 +0100

    Stretch: List of Projects on React app

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
+import Projects from './components/Projects';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			users: []
+			projects: []
 		};
+	}
+
+	componentDidMount() {
+		axios
+			.get('http://localhost:9090/api/projects')
+			.then((response) => {
+				this.setState(() => ({ projects: response.data }));
+			})
+			.catch((err) => console.log(err));
 	}
 
 	render() {
 		return (
 			<div className="App">
 				<h2>Projects</h2>
+				{this.state.projects.map((project) => {
+					return <Projects description={project.description} key={project.id} />;
+				})}
 			</div>
 		);
 	}
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const server = express();
 const projectsRouter = require('./Routers/projectsRouter');
 const actionsRouter = require('./Routers/actionsRouter.js');
-//const cors = require('cors');
+const cors = require('cors');
 
 server.use(express.json());
-//server.use(cors());
+server.use(cors());
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
